34acfd64a13beb5b0ef33fde290c1ba6a0a3353f
Umar-Farooq-2112 <umarfarooq211203@gmail.com>
2026-01-14 21:28:14 +0500

    Paths fixed for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Hubble-AI-Portfolio/',
+  base: '/',  // ✅ must be '/' for custom domain
   plugins: [react()]
 })
-
